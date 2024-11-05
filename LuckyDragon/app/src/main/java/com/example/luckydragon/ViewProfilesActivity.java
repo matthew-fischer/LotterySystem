90367a03a1f187228491b112e6fbf46e01f42cb2
Arman <astiwana@ualberta.ca>
2024-11-04 23:28:38 -0700

    create UserArrayAdapter

--- LuckyDragon/app/src/main/java/com/example/luckydragon/ViewProfilesActivity.java
+++ LuckyDragon/app/src/main/java/com/example/luckydragon/ViewProfilesActivity.java
@@ -1,4 +1,19 @@
 package com.example.luckydragon;
 
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ViewProfilesActivity extends AppBarActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_profiles);
+        getSupportActionBar().setTitle("Profiles");
+
+    }
 }
