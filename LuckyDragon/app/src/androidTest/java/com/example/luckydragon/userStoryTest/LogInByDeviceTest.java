3c0af9e552f32bfbde6cbf856c640d7bed188931
Ellis McDougald <ecmcdougald@gmail.com>
2024-11-07 12:45:22 -0700

    Write test for login by device

--- LuckyDragon/app/src/androidTest/java/com/example/luckydragon/userStoryTest/LogInByDeviceTest.java
+++ LuckyDragon/app/src/androidTest/java/com/example/luckydragon/userStoryTest/LogInByDeviceTest.java
@@ -3,9 +3,8 @@ package com.example.luckydragon.userStoryTest;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.isDialog;
-import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.not;
@@ -18,13 +17,8 @@ import static org.mockito.MockitoAnnotations.openMocks;
 
 import android.content.Context;
 import android.content.Intent;
-import android.view.View;
-import android.widget.Space;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -42,7 +36,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -51,7 +44,7 @@ import org.mockito.Mock;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EditFacilityTest {
+public class LogInByDeviceTest {
     @Mock
     private FirebaseFirestore mockFirestore;
     // User mocks
@@ -84,7 +77,6 @@ public class EditFacilityTest {
         // Personal info
         testUserData.put("name", "John Doe");
         testUserData.put("email", "jdoe@ualberta.ca");
-        testUserData.put("phoneNumber", "780-831-3291");
         // Roles
         testUserData.put("isEntrant", true);
         testUserData.put("isOrganizer", true);
@@ -146,82 +138,32 @@ public class EditFacilityTest {
 
     /**
      * USER STORY TEST
-     * User opens app and selects "Organizer".
-     * User already has a facility.
-     * The user's existing facility is displayed.
-     * User clicks edit button and EditFacilityDialogFragment is started.
-     * The text input field is the user's existing facility.
-     * User enters a new facility name and selects confirm.
-     * EditFacilityDialogFragment closes.
-     * The change is reflected on the organizer's profile.
+     * User opens app and selects 'entrant'.
+     * They are brought straight to their profile.
+     * There is no login page.
      */
     @Test
-    public void testSetFacilityNotFirstTime() {
+    public void loginByDeviceTest() {
         final Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         final Intent intent = new Intent(targetContext, SelectRoleActivity.class);
 
         GlobalApp globalApp = (GlobalApp) targetContext.getApplicationContext();
         globalApp.setDb(mockFirestore);
 
-        try(final ActivityScenario<SelectRoleActivity> scenario = ActivityScenario.launch(intent)) {
+        try (final ActivityScenario<SelectRoleActivity> scenario = ActivityScenario.launch(intent)) {
             // User is not admin, so admin button should not show
             onView(ViewMatchers.withId(R.id.entrantButton)).check(matches(isDisplayed()));
             onView(withId(R.id.organizerButton)).check(matches(isDisplayed()));
             onView(withId(R.id.adminButton)).check(matches(not(isDisplayed())));
 
-            // User clicks "Organizer"
-            onView(withId(R.id.organizerButton)).perform(click());
+            // User clicks "Entrant"
+            onView(withId(R.id.entrantButton)).perform(click());
 
-            // Profile activity should open and organizer profile should be displayed
-            onView(withId(R.id.organizerProfileLayout)).check(matches(isDisplayed()));
-
-            // The organizer's facility is displayed
-            onView(withId(R.id.facilityTextView)).check(matches(withText("The Sports Centre")));
-
-            // Before clicking the facility edit button, hide the space because it causes weird espresso behaviour
-            onView(withId(R.id.facilitySpace)).perform(setSpaceVisibility(false));
-
-            // User clicks facility edit button
-            onView(withId(R.id.facilityEditButton)).perform(click());
-
-            // Edit Facility dialog should be displayed now
-            onView(withId(R.id.editFacilityDialog)).check(matches(isDisplayed()));
-
-            // The facility edittext is the previous facility
-            onView(withId(R.id.edit_facility_FacilityEditText)).check(matches(withText("The Sports Centre")));
-
-            // Set edittext to test facility
-            onView(withId(R.id.edit_facility_FacilityEditText)).perform(ViewActions.clearText());
-            onView(withId(R.id.edit_facility_FacilityEditText)).perform(ViewActions.typeText("The Event Centre"));
-
-            // Click confirm
-            onView(withText("Confirm"))
-                    .inRoot(isDialog())
-                    .check(matches(isDisplayed()))
-                    .perform(click());
-
-            // Check that facility text matches the new facility name
-            onView(withId(R.id.facilityTextView)).check(matches(withText("The Event Centre")));
+            // User is brought straight to their profile
+            onView(withId(R.id.nameTextView)).check(matches(withText("John Doe")));
+            onView(withId(R.id.emailTextView)).check(matches(withText("jdoe@ualberta.ca")));
+            // User has no phone number so phone number shouldn't show
+            onView(withId(R.id.phoneNumberTextView)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
         }
-    }
-
-    private static ViewAction setSpaceVisibility(final boolean value) {
-        return new ViewAction() {
-
-            @Override
-            public Matcher<View> getConstraints() {
-                return isAssignableFrom(Space.class);
-            }
-
-            @Override
-            public void perform(UiController uiController, View view) {
-                view.setVisibility(value ? View.VISIBLE : View.GONE);
-            }
-
-            @Override
-            public String getDescription() {
-                return "Show / Hide View";
-            }
-        };
     }
 }
