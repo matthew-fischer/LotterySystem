30c785089cd4ff102db00fd3740244874ba60ab7
Ahmed Siadomar <ahmedsiadsemail@gmail.com>
2024-11-08 12:46:29 -0700

    Fix failing test

--- LuckyDragon/app/src/androidTest/java/com/example/luckydragon/userStoryTest/EventGeolocationWarning.java
+++ LuckyDragon/app/src/androidTest/java/com/example/luckydragon/userStoryTest/EventGeolocationWarning.java
@@ -50,12 +50,12 @@ public class EventGeolocationWarning extends MockedDb {
         eventData.put("name", "C301 Standup");
         eventData.put("organizerDeviceId", "mockOrgId");
         eventData.put("facility", "UofA");
-        eventData.put("waitListLimit", 10);
-        eventData.put("attendeeLimit", 10);
+        eventData.put("waitListLimit", new Long(10));
+        eventData.put("attendeeLimit", new Long(10));
         eventData.put("hasGeolocation", true);
         eventData.put("date", LocalDate.now().toString());
-        eventData.put("hours", 10);
-        eventData.put("minutes", 30);
+        eventData.put("hours", new Long(10));
+        eventData.put("minutes", new Long(30));
         eventData.put("hashedQR", "Fake QR");
         eventData.put("waitList", new ArrayList<>());
         eventData.put("inviteeList", new ArrayList<>());
