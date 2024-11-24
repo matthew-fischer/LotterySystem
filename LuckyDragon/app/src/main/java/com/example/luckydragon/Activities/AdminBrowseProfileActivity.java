be6f08449277b879c822a04302b5e157d644b7e9
Arman <astiwana@ualberta.ca>
2024-11-23 18:31:50 -0700

    add admin browse profile activity with basic setup

--- LuckyDragon/app/src/main/java/com/example/luckydragon/Activities/AdminBrowseProfileActivity.java
+++ LuckyDragon/app/src/main/java/com/example/luckydragon/Activities/AdminBrowseProfileActivity.java
@@ -1,4 +1,23 @@
 package com.example.luckydragon.Activities;
 
-public class AdminBrowseProfileActivity {
+import android.os.Bundle;
+
+import com.example.luckydragon.GlobalApp;
+import com.example.luckydragon.Models.User;
+import com.example.luckydragon.R;
+import com.example.luckydragon.Views.ProfileView;
+
+public class AdminBrowseProfileActivity extends AppBarActivity{
+
+    private User user;
+    private GlobalApp.ROLE role;
+    private ProfileView profileView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_browse_profile);
+        getSupportActionBar().setTitle("Profile Management");
+    }
+
 }
