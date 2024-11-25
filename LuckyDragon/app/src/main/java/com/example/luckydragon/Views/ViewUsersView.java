e3bec17f1981d5a05ddb53d140e988b754b98884
Arman <astiwana@ualberta.ca>
2024-11-24 17:10:38 -0700

    create constructor for the view users view

--- LuckyDragon/app/src/main/java/com/example/luckydragon/Views/ViewUsersView.java
+++ LuckyDragon/app/src/main/java/com/example/luckydragon/Views/ViewUsersView.java
@@ -1,8 +1,14 @@
 package com.example.luckydragon.Views;
 
 import com.example.luckydragon.Activities.ViewProfilesActivity;
+import com.example.luckydragon.Models.UserList;
 
 public class ViewUsersView extends Observer{
 
-    private final ViewProfilesActivity;
+    private final ViewProfilesActivity viewProfilesActivity;
+
+    public ViewUsersView(UserList users, ViewProfilesActivity viewProfilesActivity) {
+        this.viewProfilesActivity = viewProfilesActivity;
+        startObserving(users);
+    }
 }
