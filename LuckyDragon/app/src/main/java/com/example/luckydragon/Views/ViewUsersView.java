996ebec7a1b58ca1d6bb265b113ea62491a60c5a
Arman <astiwana@ualberta.ca>
2024-11-24 17:08:54 -0700

    create ViewUsersView

---
+++ LuckyDragon/app/src/main/java/com/example/luckydragon/Views/ViewUsersView.java
@@ -0,0 +1,8 @@
+package com.example.luckydragon.Views;
+
+import com.example.luckydragon.Activities.ViewProfilesActivity;
+
+public class ViewUsersView extends Observer{
+
+    private final ViewProfilesActivity;
+}
