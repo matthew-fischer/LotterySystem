67110d5be2b9f00abae084faa8ec941586aff1f4
Arman <astiwana@ualberta.ca>
2024-11-24 17:14:04 -0700

    created method that returns the observed UserList

--- LuckyDragon/app/src/main/java/com/example/luckydragon/Views/ViewUsersView.java
+++ LuckyDragon/app/src/main/java/com/example/luckydragon/Views/ViewUsersView.java
@@ -1,6 +1,7 @@
 package com.example.luckydragon.Views;
 
 import com.example.luckydragon.Activities.ViewProfilesActivity;
+import com.example.luckydragon.Models.Observable;
 import com.example.luckydragon.Models.UserList;
 
 public class ViewUsersView extends Observer{
@@ -10,5 +11,17 @@ public class ViewUsersView extends Observer{
     public ViewUsersView(UserList users, ViewProfilesActivity viewProfilesActivity) {
         this.viewProfilesActivity = viewProfilesActivity;
         startObserving(users);
+    }
+
+    @Override
+    public UserList getObservable() {
+        return (UserList) super.getObservable();
+    }
+
+    @Override
+    public void update(Observable whoUpdatedMe) {
+
+        viewProfilesActivity.notifyAdapter();
+
     }
 }
