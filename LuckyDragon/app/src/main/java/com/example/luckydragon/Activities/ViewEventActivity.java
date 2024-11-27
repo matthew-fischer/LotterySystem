fd1acc91acd32ea5bfffc0e936877d1ce6936674
jerryxu20 <xu_jerry@outlook.com>
2024-11-27 14:19:13 -0700

    add import

--- LuckyDragon/app/src/main/java/com/example/luckydragon/Activities/ViewEventActivity.java
+++ LuckyDragon/app/src/main/java/com/example/luckydragon/Activities/ViewEventActivity.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import com.example.luckydragon.Fragments.AdminEventFragment;
+import com.example.luckydragon.Fragments.DisplayImageFragment;
 import com.example.luckydragon.Fragments.DisplayQRCodeFragment;
 import com.example.luckydragon.Fragments.EntrantEventAttendingFragment;
 import com.example.luckydragon.Fragments.EntrantEventInvitedFragment;
