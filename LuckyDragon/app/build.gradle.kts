a525ffd218971772df8a1c6b81c1e2e59036d402
Ellis McDougald <ecmcdougald@gmail.com>
2024-12-02 10:28:09 -0700

    Generates new javadocs

--- LuckyDragon/app/build.gradle.kts
+++ LuckyDragon/app/build.gradle.kts
@@ -47,7 +47,6 @@ android {
 }
 
 dependencies {
-//    implementation(libs.material.v130)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
@@ -56,30 +55,22 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.firebase.firestore)
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.ext.junit)
-//    androidTestImplementation(libs.espresso.core)
-    // noinspection UseTomlInstead
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.zxing:core:3.3.0")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation(libs.play.services.location)
     androidTestImplementation(libs.junit.jupiter)
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
-
     testImplementation("org.mockito:mockito-core:5.14.2")
     testImplementation("org.mockito:mockito-core:5.14.2")
     testImplementation("androidx.test.ext:junit:1.2.1")
     testImplementation(libs.junit.jupiter);
-
     androidTestImplementation("org.mockito:mockito-core:5.14.2")
     androidTestImplementation("org.mockito:mockito-android:5.14.2")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-
     implementation("org.osmdroid:osmdroid-android:6.1.20")
 }
